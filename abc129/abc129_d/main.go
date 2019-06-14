21043fce83f377adbf47e322d1543d923c3b1ac4
Toshinori Tsugita <tsugita.dev@gmail.com>
2019-06-14 22:42:54 +0900

    Fix TLE of abc129 d (Scanf is too slow)

--- abc129/abc129_d/main.go
+++ abc129/abc129_d/main.go
@@ -2,7 +2,30 @@ package main
 
 import (
 	"fmt"
+	"bufio"
+	"os"
 )
+
+var rdr = bufio.NewReaderSize(os.Stdin, 1000000)
+
+func readLine() string {
+	buf := make([]byte, 0, 1000000)
+
+	for {
+		l, p, e := rdr.ReadLine()
+		if e != nil {
+			panic(e)
+		}
+
+		buf = append(buf, l...)
+
+		if !p {
+			break
+		}
+	}
+
+	return string(buf)
+}
 
 func solve(H, W int, rows []string) int {
 	h := make([][]int, H)
@@ -15,49 +38,49 @@ func solve(H, W int, rows []string) int {
 
 	for i := 0; i < H; i++ {
 		ct := 0
-		for j := 0; j < W; j++ {
+		from := 0
+		for j := 0; j <= W; j++ {
+			if j == W || rows[i][j] == '#' {
+				for k := from; k < j; k++ {
+					h[i][k] = ct
+				}
+			}
+
+			if j == W {
+				continue
+			}
+
 			if rows[i][j] == '#' {
+				h[i][j] = 0
 				ct = 0
+				from = j+1
 			} else {
 				ct++
 			}
-
-			h[i][j] = ct
-		}
-
-		ct = 0
-		for j := W - 1; j >= 0; j-- {
-			if rows[i][j] == '#' {
-				ct = 0
-			} else {
-				ct++
-			}
-
-			h[i][j] += ct
 		}
 	}
 
 	for j := 0; j < W; j++ {
 		ct := 0
-		for i := 0; i < H; i++ {
+		from := 0
+		for i := 0; i <= H; i++ {
+			if i == H || rows[i][j] == '#' {
+				for k := from; k < i; k++ {
+					v[k][j] = ct
+				}
+			}
+
+			if i == H {
+				continue
+			}
+
 			if rows[i][j] == '#' {
+				v[i][j] = 0
 				ct = 0
+				from = i+1
 			} else {
 				ct++
 			}
-
-			v[i][j] = ct
-		}
-
-		ct = 0
-		for i := H - 1; i >= 0; i-- {
-			if rows[i][j] == '#' {
-				ct = 0
-			} else {
-				ct++
-			}
-
-			v[i][j] += ct
 		}
 	}
 
@@ -65,7 +88,7 @@ func solve(H, W int, rows []string) int {
 
 	for i := 0; i < H; i++ {
 		for j := 0; j < W; j++ {
-			res := h[i][j] + v[i][j] - 3
+			res := h[i][j] + v[i][j] - 1
 
 			if res > max {
 				max = res
@@ -85,7 +108,7 @@ func main() {
 	rows := make([]string, H)
 
 	for i := 0; i < H; i++ {
-		fmt.Scanf("%s", &rows[i])
+		rows[i] = readLine()
 	}
 
 	output := solve(H, W, rows)
