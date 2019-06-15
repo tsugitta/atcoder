f7013d4f90a93d66002f7d937b93281b13ff5c00
Toshinori Tsugita <tsugita.dev@gmail.com>
2019-06-15 20:52:05 +0900

    Rewrite abc129_d test with yaml test cases

--- abc129/abc129_d/main_test.go
+++ abc129/abc129_d/main_test.go
@@ -6,7 +6,28 @@ import (
 	"fmt"
 	"strings"
 	"testing"
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v2"
 )
+
+type TestCase struct {
+	In string
+	Out string
+}
+
+func YamlBufToTestCases(buf []byte) ([]TestCase, error) {
+	cases := make([]TestCase, 10)
+	err := yaml.Unmarshal(buf, &cases)
+
+	if err != nil {
+			fmt.Println(err)
+			return nil, err
+	}
+
+	return cases, nil
+}
 
 func NewMockIo(input string, outputBuffer *bytes.Buffer) *Io {
 	return &Io{
@@ -16,40 +37,25 @@ func NewMockIo(input string, outputBuffer *bytes.Buffer) *Io {
 }
 
 func TestSolve(t *testing.T) {
-	cases := []struct {
-		input    string
-		expected string
-	}{
-		{
-			input: `4 6
-#..#..
-.....#
-....#.
-#.#...`,
-			expected: "8\n",
-		}, {
-			input: `8 8
-..#...#.
-....#...
-##......
-..###..#
-...#..#.
-##....#.
-#...#...
-###.#..#`,
-			expected: "13\n"},
+	yamlBuf, err := ioutil.ReadFile("./test.yaml")
+
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	cases, err := YamlBufToTestCases(yamlBuf)
 
 	for idx, c := range cases {
 		t.Run(fmt.Sprintf("%d", idx), func(t *testing.T) {
 			buffer := bytes.Buffer{}
 
-			io := NewMockIo(c.input, &buffer)
+			io := NewMockIo(c.In, &buffer)
 			solve(io)
 			io.Flush()
 
-			if actual := buffer.String(); actual != c.expected {
-				t.Fatalf("expected: %#v, actual: %#v", c.expected, actual)
+			output := strings.TrimSuffix(buffer.String(), "\n")
+			if output != c.Out {
+				t.Fatalf("expected: %#v, actual: %#v", c.Out, output)
 			}
 		})
 	}
