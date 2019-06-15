b5657087b28cbf84422312d66837a256d21c98a5
Toshinori Tsugita <tsugita.dev@gmail.com>
2019-06-16 03:41:28 +0900

    Solve abc128 a

---
+++ abc128/abc128_a/main.go
@@ -0,0 +1,195 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func solve(io *Io) {
+	A := io.NextInt()
+	P := io.NextInt()
+
+	res := (A*3 + P) / 2
+
+	io.Println(res)
+}
+
+func main() {
+	io := NewIo()
+	defer io.Flush()
+	solve(io)
+}
+
+/* IO Helpers */
+
+// Io combines reader, writer, & tokens as the state when processing the input
+type Io struct {
+	reader    *bufio.Reader
+	writer    *bufio.Writer
+	tokens    []string
+	nextToken int
+}
+
+// NewIo creates Io with Stdin & Stdout
+func NewIo() *Io {
+	return &Io{
+		reader: bufio.NewReader(os.Stdin),
+		writer: bufio.NewWriter(os.Stdout),
+	}
+}
+
+// Flush calls the writer's Flush
+func (io *Io) Flush() {
+	io.writer.Flush()
+}
+
+// NextLine returns the string from reader.ReadLine()
+func (io *Io) NextLine() string {
+	if io.nextToken < len(io.tokens) {
+		panic("io.nextToken < len(io.tokens)")
+	}
+
+	var buffer bytes.Buffer
+
+	for {
+		line, isPrefix, err := io.reader.ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		buffer.Write(line)
+		if !isPrefix {
+			break
+		}
+	}
+
+	return buffer.String()
+}
+
+// NextLines returns []string from next n lines
+func (io *Io) NextLines(n int) []string {
+	res := make([]string, n)
+	for i := 0; i < n; i++ {
+		res[i] = io.NextLine()
+	}
+	return res
+}
+
+// Next returns the string token (partial string of the line divided by spaces)
+func (io *Io) Next() string {
+	if io.nextToken >= len(io.tokens) {
+		line := io.NextLine()
+		io.tokens = strings.Fields(line)
+		io.nextToken = 0
+	}
+
+	res := io.tokens[io.nextToken]
+	io.nextToken++
+	return res
+}
+
+// NextStrings returns the []string from the next n tokens
+func (io *Io) NextStrings(n int) []string {
+	res := make([]string, n)
+	for i := 0; i < n; i++ {
+		res[i] = io.Next()
+	}
+	return res
+}
+
+// NextInt returns the int from the next token
+func (io *Io) NextInt() int {
+	res, err := strconv.Atoi(io.Next())
+	if err != nil {
+		panic(err)
+	}
+	return res
+}
+
+// NextInts returns the []int from the next n tokens
+func (io *Io) NextInts(n int) []int {
+	res := make([]int, n)
+	for i := 0; i < n; i++ {
+		res[i] = io.NextInt()
+	}
+	return res
+}
+
+// Println prints the input to the writer in an easy-to-see form
+func (io *Io) Println(a ...interface{}) {
+	var values []string
+	for i := 0; i < len(a); i++ {
+		values = append(values, "%v")
+	}
+	io.Printfln(strings.Join(values, " "), a...)
+}
+
+// Print calls Fprint to the writer
+func (io *Io) Print(a interface{}) {
+	fmt.Fprint(io.writer, a)
+}
+
+// Printfln calls Fprint to the writer
+func (io *Io) Printfln(format string, a ...interface{}) {
+	fmt.Fprintf(io.writer, format+"\n", a...)
+}
+
+/* Math Helpers */
+
+// Max of the ints
+func Max(numbers ...int) int {
+	max := numbers[0]
+	for i, number := range numbers {
+		if i == 0 {
+			continue
+		}
+		if number > max {
+			max = number
+		}
+	}
+	return max
+}
+
+// Min of the ints
+func Min(numbers ...int) int {
+	max := numbers[0]
+	for i, number := range numbers {
+		if i == 0 {
+			continue
+		}
+		if max > number {
+			max = number
+		}
+	}
+	return max
+}
+
+// DigitSum returns sum of the int's digits
+func DigitSum(n int) int {
+	if n < 0 {
+		return -1
+	}
+
+	res := 0
+
+	for n > 0 {
+		res += n % 10
+		n /= 10
+	}
+
+	return res
+}
+
+// Sum of the ints
+func Sum(numbers ...int) int {
+	sum := 0
+
+	for _, number := range numbers {
+		sum += number
+	}
+
+	return sum
+}
---
+++ abc128/abc128_a/main_test.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+	"testing"
+
+	"gopkg.in/yaml.v2"
+)
+
+type SolveFunction = func(io *Io)
+
+var solveFuncMap map[string]SolveFunction
+
+func TestMain(m *testing.M) {
+	solveFuncMap = map[string]SolveFunction{
+		// Add solve functions here to test another solution
+		"solve": solve,
+	}
+
+	exit := m.Run()
+
+	if exit != 0 {
+		os.Exit(exit)
+	}
+}
+
+type TestCase struct {
+	In  string
+	Out string
+}
+
+func YamlBufToTestCases(buf []byte) ([]TestCase, error) {
+	cases := make([]TestCase, 10)
+	err := yaml.Unmarshal(buf, &cases)
+
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	return cases, nil
+}
+
+func NewMockIo(input string, outputBuffer *bytes.Buffer) *Io {
+	return &Io{
+		reader: bufio.NewReader(strings.NewReader(input)),
+		writer: bufio.NewWriter(outputBuffer),
+	}
+}
+
+func TestSolve(t *testing.T) {
+	yamlBuf, err := ioutil.ReadFile("./test.yaml")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cases, err := YamlBufToTestCases(yamlBuf)
+
+	for funcName, solveFunc := range solveFuncMap {
+		for idx, c := range cases {
+			t.Run(fmt.Sprintf("%s %d", funcName, idx), func(t *testing.T) {
+				buffer := bytes.Buffer{}
+
+				io := NewMockIo(c.In, &buffer)
+				solveFunc(io)
+				io.Flush()
+
+				output := strings.TrimSuffix(buffer.String(), "\n")
+				if output != c.Out {
+					t.Fatalf("expected: %#v, actual: %#v", c.Out, output)
+				}
+			})
+		}
+	}
+}
