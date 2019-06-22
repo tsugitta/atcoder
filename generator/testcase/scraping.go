34d1bb559db51bad25352ba1c938692172cc2f17
Toshinori Tsugita <tsugita.dev@gmail.com>
2019-06-22 23:24:25 +0900

    Improve fragile test-case scraping logic

--- generator/testcase/scraping.go
+++ generator/testcase/scraping.go
@@ -1,30 +1,45 @@
 package testcase
 
 import (
+	"strings"
+
 	"github.com/gocolly/colly"
 	"github.com/pkg/errors"
 )
 
 func scrapeTestCases(URL string) ([]*TestCase, error) {
 	c := colly.NewCollector(
+		colly.UserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36"),
 		colly.AllowedDomains("atcoder.jp"),
 	)
 
 	inputs := []string{}
 	outputs := []string{}
-	elementCount := 0
 
-	c.OnHTML("#task-statement .lang-ja pre", func(e *colly.HTMLElement) {
-		elementCount++
+	c.OnHTML("#task-statement .lang-ja div.part", func(e *colly.HTMLElement) {
+		isInputExample := false
+		isOutputExample := false
 
-		if elementCount == 1 {
-			// this is not a test case, but a description about input / output
+		// only one h3 is included
+		e.ForEach("h3", func(i int, he *colly.HTMLElement) {
+			if strings.Contains(e.Text, "入力例") {
+				isInputExample = true
+			} else if strings.Contains(e.Text, "出力例") {
+				isOutputExample = true
+			}
+		})
+
+		if !isInputExample && !isOutputExample {
 			return
-		} else if elementCount%2 == 0 {
-			inputs = append(inputs, e.Text)
-		} else {
-			outputs = append(outputs, e.Text)
 		}
+
+		e.ForEach("pre", func(i int, pe *colly.HTMLElement) {
+			if isInputExample {
+				inputs = append(inputs, pe.Text)
+			} else if isOutputExample {
+				outputs = append(outputs, pe.Text)
+			}
+		})
 	})
 
 	err := c.Visit(URL)
