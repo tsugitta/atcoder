e6e39441ab407a1560c1a5b3fca026e80f0011f5
Toshinori Tsugita <tsugita.dev@gmail.com>
2019-08-25 19:01:45 +0900

    Solve m-solutions2019 a~e

---
+++ m-solutions2019/m_solutions2019_c/main.go
@@ -0,0 +1,416 @@
+// https://atcoder.jp/contests/m-solutions2019/tasks/m_solutions2019_c
+
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type CombinationTable struct {
+	// i! % MOD M
+	fact []int
+	// (i!)^(-1) MOD M
+	finv []int
+	// i^(-1) MOD M
+	inv []int
+
+	mod int
+}
+
+// ref: http://drken1215.hatenablog.com/entry/2018/06/08/210000
+func prepareCombination(max int, mod int) CombinationTable {
+	fact := make([]int, max+1)
+	finv := make([]int, max+1)
+	inv := make([]int, max+1)
+
+	fact[0] = 1
+	fact[1] = 1
+	finv[0] = 1
+	finv[1] = 1
+	inv[1] = 1
+
+	for i := 2; i <= max; i++ {
+		fact[i] = fact[i-1] * i % mod
+		inv[i] = mod - inv[mod%i]*(mod/i)%mod // a^(-1) ≡ -(p % a)^(-1) * (p/a) MOD p
+		finv[i] = finv[i-1] * inv[i] % mod
+	}
+
+	return CombinationTable{
+		fact: fact,
+		finv: finv,
+		inv:  inv,
+		mod:  mod,
+	}
+}
+
+func combinationM(n, k int, table CombinationTable) int {
+	if n < k {
+		return 0
+	}
+	if n < 0 || k < 0 {
+		return 0
+	}
+
+	return table.fact[n] * (table.finv[k] * table.finv[n-k] % table.mod) % table.mod
+}
+
+// PowM gets the Pow value mod M
+func PowM(a, e, mod int) int {
+	res := 1
+
+	for e != 0 {
+		if e&1 == 1 {
+			res = res * a % mod
+		}
+		a = a * a % mod
+		e = e >> 1
+	}
+
+	return res
+}
+
+// d: debug IO. it can print debug in test.
+func solve(io *Io, d *Io) {
+	N := io.NextInt()
+	A := io.NextInt()
+	B := io.NextInt()
+	C := io.NextInt()
+
+	ct := prepareCombination(Max(2*N-1, 100), MOD)
+
+	// なお、M 回でゲームが終わる確率は、 M 回中引き分け以外が L 回起きる確率と L 回引き分け以外が起きた時における N 回で勝負が終わる条件付き確率の積の和
+	// これに M をかけ級数を取ったのが求める期待値。
+	// ここで和と級数の順番を入れ替え「L 回引き分け以外が起きたときに N 回で勝負が終わる条件付き確率」*「L~∞ 回のゲームで L 回引き分け以外がおきる確率」とする
+	// また、確率変数の M （L~∞）を後者の確率に含めると、後者は L 回引き分け以外が起きるまでにかかる回数の期待値となる
+	// よってそれぞれを別々に求めれば良い
+	// ところで後者について、 1 回引き分け以外が起きるまでにかかる回数の期待値は幾何分布の期待値で 1/(引き分け以外の確率)で、和の期待値として
+	// L/(引き分け以外の確率) となる
+	// 1/(引き分け以外の確率 )= 1 / ((100-C) / 100) = 100 / (100-C)
+
+	pa := A * ct.inv[A+B] % MOD
+	pb := B * ct.inv[A+B] % MOD
+
+	res := 0
+
+	for m := N; m <= 2*N-1; m++ {
+		r := PowM(pa, N, MOD)*PowM(pb, m-N, MOD)%MOD + PowM(pb, N, MOD)*PowM(pa, m-N, MOD)%MOD
+		r %= MOD
+		r *= combinationM(m-1, N-1, ct)
+		r %= MOD
+		r *= m
+		r %= MOD
+
+		res += r
+		res %= MOD
+	}
+
+	res = res * 100 % MOD * ct.inv[100-C] % MOD // * 100/(100-C)
+	res %= MOD
+	io.Println(res)
+}
+
+func main() {
+	io := NewIo()
+	defer io.Flush()
+	solve(io, nil)
+}
+
+/* IO Helpers */
+
+// Io combines reader, writer, & tokens as the state when processing the input
+type Io struct {
+	reader    *bufio.Reader
+	writer    *bufio.Writer
+	tokens    []string
+	nextToken int
+}
+
+// NewIo creates Io with Stdin & Stdout
+func NewIo() *Io {
+	return &Io{
+		reader: bufio.NewReader(os.Stdin),
+		writer: bufio.NewWriter(os.Stdout),
+	}
+}
+
+// Flush calls the writer's Flush
+func (io *Io) Flush() {
+	io.writer.Flush()
+}
+
+// NextLine returns the string from reader.ReadLine()
+func (io *Io) NextLine() string {
+	if io.nextToken < len(io.tokens) {
+		panic("io.nextToken < len(io.tokens)")
+	}
+
+	var buffer bytes.Buffer
+
+	for {
+		line, isPrefix, err := io.reader.ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		buffer.Write(line)
+		if !isPrefix {
+			break
+		}
+	}
+
+	return buffer.String()
+}
+
+// NextLines returns []string from next n lines
+func (io *Io) NextLines(n int) []string {
+	res := make([]string, n)
+	for i := 0; i < n; i++ {
+		res[i] = io.NextLine()
+	}
+	return res
+}
+
+// Next returns the string token (partial string of the line divided by spaces)
+func (io *Io) Next() string {
+	if io.nextToken >= len(io.tokens) {
+		line := io.NextLine()
+		io.tokens = strings.Fields(line)
+		io.nextToken = 0
+	}
+
+	res := io.tokens[io.nextToken]
+	io.nextToken++
+	return res
+}
+
+// NextStrings returns the []string from the next n tokens
+func (io *Io) NextStrings(n int) []string {
+	res := make([]string, n)
+	for i := 0; i < n; i++ {
+		res[i] = io.Next()
+	}
+	return res
+}
+
+// NextRunes returns next string as []rune
+func (io *Io) NextRunes() []rune {
+	return []rune(io.Next())
+}
+
+// NextInt returns the int from the next token
+func (io *Io) NextInt() int {
+	res, err := strconv.Atoi(io.Next())
+	if err != nil {
+		panic(err)
+	}
+	return res
+}
+
+// NextInts returns the []int from the next n tokens
+func (io *Io) NextInts(n int) []int {
+	res := make([]int, n)
+	for i := 0; i < n; i++ {
+		res[i] = io.NextInt()
+	}
+	return res
+}
+
+// NextFloat returns the float64 from the next token
+func (io *Io) NextFloat() float64 {
+	res, err := strconv.ParseFloat(io.Next(), 64)
+	if err != nil {
+		panic(err)
+	}
+	return res
+}
+
+// NextFloats returns the []float64 from the next n tokens
+func (io *Io) NextFloats(n int) []float64 {
+	res := make([]float64, n)
+	for i := 0; i < n; i++ {
+		res[i] = io.NextFloat()
+	}
+	return res
+}
+
+// Println prints the input to the writer in an easy-to-see form
+func (io *Io) Println(a ...interface{}) {
+	var values []string
+	for i := 0; i < len(a); i++ {
+		values = append(values, "%v")
+	}
+	io.Printfln(strings.Join(values, " "), a...)
+}
+
+// Print calls Fprint to the writer
+func (io *Io) Print(a interface{}) {
+	fmt.Fprint(io.writer, a)
+}
+
+// Printf calls Fprintf to the writer
+func (io *Io) Printf(format string, a ...interface{}) {
+	fmt.Fprintf(io.writer, format, a...)
+}
+
+// Printfln calls Fprint to the writer
+func (io *Io) Printfln(format string, a ...interface{}) {
+	fmt.Fprintf(io.writer, format+"\n", a...)
+}
+
+// PrintInts prints ints with space and new line at the end
+func (io *Io) PrintInts(ints []int) {
+	for i, e := range ints {
+		io.Print(e)
+
+		if i == len(ints)-1 {
+			io.Println()
+		} else {
+			io.Print(" ")
+		}
+	}
+}
+
+// Debug calls Println and Flush immediately
+func (io *Io) Debug(a ...interface{}) {
+	if io == nil {
+		return
+	}
+
+	io.Println(a...)
+	io.Flush()
+}
+
+/* Math Helpers */
+
+// MOD is 10^9 + 7
+const MOD = 1000000007
+
+// Max of the ints
+func Max(numbers ...int) int {
+	max := numbers[0]
+	for i, number := range numbers {
+		if i == 0 {
+			continue
+		}
+		if number > max {
+			max = number
+		}
+	}
+	return max
+}
+
+// Min of the ints
+func Min(numbers ...int) int {
+	min := numbers[0]
+	for i, number := range numbers {
+		if i == 0 {
+			continue
+		}
+		if min > number {
+			min = number
+		}
+	}
+	return min
+}
+
+// Sum of the ints
+func Sum(numbers ...int) int {
+	sum := 0
+
+	for _, number := range numbers {
+		sum += number
+	}
+
+	return sum
+}
+
+// Pow calculates the pow for int with O(log e)
+func Pow(a, e int) int {
+	if a < 0 || e < 0 {
+		panic(fmt.Sprintf("Pow was called for a < 0 or e < 0, a: %d, e: %d", a, e))
+	}
+
+	if e == 0 {
+		return 1
+	}
+
+	if e%2 == 0 {
+		half := Pow(a, e/2)
+		return half * half
+	}
+
+	return a * Pow(a, e-1)
+}
+
+// Abs returns the absolute value of the input
+func Abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+
+	return a
+}
+
+/* Structure Helpers */
+
+// Set is orderless List
+type Set map[interface{}]struct{}
+
+// NewSet returns empty Set
+func NewSet() *Set {
+	return &Set{}
+}
+
+// Add the value to the set
+func (set *Set) Add(value interface{}) {
+	(*set)[value] = struct{}{}
+}
+
+// Includes check the set has the value
+func (set *Set) Includes(value interface{}) bool {
+	_, included := (*set)[value]
+	return included
+}
+
+// Remove deletes the value from the set if exists
+func (set *Set) Remove(value interface{}) {
+	if !set.Includes(value) {
+		return
+	}
+
+	delete(*set, value)
+}
+
+// ImmutableSort returns sorted slice without mutating original one
+func ImmutableSort(slice []int) []int {
+	res := make([]int, len(slice))
+	copy(res, slice)
+	sort.Ints(res)
+	return res
+}
+
+/* Util helpers */
+
+// Ternary is like `cond ? a : b`. should assert the returned value like `Ternary(cond, a, b).(int)`
+func Ternary(cond bool, a, b interface{}) interface{} {
+	if cond {
+		return a
+	}
+
+	return b
+}
+
+// TernaryInt uses Ternary and assert the value as int
+func TernaryInt(cond bool, a, b int) int {
+	return Ternary(cond, a, b).(int)
+}
+
+// TernaryString uses Ternary and assert the value as string
+func TernaryString(cond bool, a, b string) string {
+	return Ternary(cond, a, b).(string)
+}
