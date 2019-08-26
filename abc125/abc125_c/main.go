e7cfdb714f80ce7ebc61a94b55552600c7a1a3a2
Toshinori Tsugita <tsugita.dev@gmail.com>
2019-08-26 23:02:37 +0900

    Solve abc125 c with SegTree

--- abc125/abc125_c/main.go
+++ abc125/abc125_c/main.go
@@ -60,10 +60,92 @@ func solve(io *Io, d *Io) {
 	io.Println(resMc.current)
 }
 
+type SegmentTree struct {
+	n         int // 最下段の数
+	nodes     []int
+	zeroValue int
+}
+
+func (st *SegmentTree) calc(a, b int) int {
+	return gcd(a, b)
+}
+
+func (st *SegmentTree) init(vs []int, zeroValue int) {
+	st.zeroValue = zeroValue
+	st.n = 1
+
+	for st.n < len(vs) {
+		st.n *= 2
+	}
+
+	st.nodes = make([]int, 2*st.n-1)
+
+	for i := range st.nodes {
+		st.nodes[i] = st.zeroValue
+	}
+
+	for i, v := range vs {
+		st.nodes[i+st.n-1] = v
+	}
+
+	for i := st.n - 2; i >= 0; i-- {
+		st.nodes[i] = st.calc(st.nodes[i*2+1], st.nodes[i*2+2])
+	}
+}
+
+func (st *SegmentTree) update(i, val int) {
+	i += st.n - 1
+	st.nodes[i] = val
+
+	for i > 0 {
+		i = (i - 1) / 2
+		st.nodes[i] = st.calc(st.nodes[2*i+1], st.nodes[2*i+2])
+	}
+}
+
+// [from, to)
+func (st *SegmentTree) query(from, to int) int {
+	return st.queryInternal(from, to, 0, 0, st.n)
+}
+
+func (st *SegmentTree) queryInternal(from, to, k, l, r int) int {
+	if r <= from || to <= l {
+		return st.zeroValue
+	}
+
+	if from <= l && r <= to {
+		return st.nodes[k]
+	}
+
+	vl := st.queryInternal(from, to, 2*k+1, l, (l+r)/2)
+	vr := st.queryInternal(from, to, 2*k+2, (l+r)/2, r)
+
+	return st.calc(vl, vr)
+}
+
+// セグメント木を利用 O(N log N)
+func solve2(io *Io, d *Io) {
+	N := io.NextInt()
+	as := io.NextInts(N)
+
+	st := SegmentTree{}
+	st.init(as, 0)
+
+	res := 0
+
+	for i := range as {
+		l := st.query(0, i)
+		r := st.query(i+1, len(as))
+		res = Max(res, gcd(l, r))
+	}
+
+	io.Println(res)
+}
+
 func main() {
 	io := NewIo()
 	defer io.Flush()
-	solve(io, nil)
+	solve2(io, nil)
 }
 
 /* IO Helpers */
