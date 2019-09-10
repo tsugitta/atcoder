d03950dcabad4fcd1c5206b3eba0815f23514eae
Toshinori Tsugita <tsugita.dev@gmail.com>
2019-09-11 03:39:05 +0900

    Can debug with rust command *
    but need to always specify program name..

--- go_snippets/ordered_set.go
+++ go_snippets/ordered_set.go
@@ -44,11 +44,11 @@ func (s *OrderedSet) Left(n int) (res int, found bool) {
 
 // n 以上の最小の要素 O(log^2 N)
 func (s *OrderedSet) Right(n int) (res int, found bool) {
-	if s.tree.Sum(n) == s.tree.Sum(s.max+1) {
+	nCt := s.tree.Sum(n) // n-1 以下の個数であることに注意
+
+	if nCt == s.tree.Sum(s.max+1) {
 		return
 	}
-
-	nCt := s.tree.Sum(n) // n-1 以下の個数であることに注意
 
 	// n ~ max で > nCt となる最小が求めるもの
 
