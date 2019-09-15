98d7593e933a67f900dc0ffa6e9850586433b69e
Toshinori Tsugita <tsugita.dev@gmail.com>
2019-09-15 22:57:12 +0900

    Solve abc141 a~e

---
+++ contests/abc141/abc141_e/main_test.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/tsugitta/atcoder/testutil"
+)
+
+type SolveFunction = func(io, debugIo *Io)
+
+var solveFuncMap map[string]SolveFunction
+
+func TestMain(m *testing.M) {
+	solveFuncMap = map[string]SolveFunction{
+		// Add solve functions here to test another solution
+		"solve":  solve,
+		"solve2": solve2,
+		"solve3": solve3,
+	}
+
+	exit := m.Run()
+
+	if exit != 0 {
+		os.Exit(exit)
+	}
+}
+
+func NewMockIo(input string, outputBuffer *bytes.Buffer) *Io {
+	return &Io{
+		reader: bufio.NewReader(strings.NewReader(input)),
+		writer: bufio.NewWriter(outputBuffer),
+	}
+}
+
+func TestSolve(t *testing.T) {
+	yamlBuf, _ := ioutil.ReadFile("./test.yaml")
+	cases, _ := testutil.YamlBufToTestCases(yamlBuf)
+
+	for funcName, solveFunc := range solveFuncMap {
+		for idx, c := range cases {
+			t.Run(fmt.Sprintf("%s %d", funcName, idx), func(t *testing.T) {
+				buffer := bytes.Buffer{}
+
+				io := NewMockIo(c.In, &buffer)
+				debugIo := NewIo()
+				debugIo.Printfln("--- Func: %s, TestCase: %d ---", funcName, idx)
+				solveFunc(io, debugIo)
+				io.Flush()
+
+				output := buffer.String()
+				if output != c.Out {
+					t.Fatalf("expected: %#v, actual: %#v", c.Out, output)
+				}
+			})
+		}
+	}
+}
