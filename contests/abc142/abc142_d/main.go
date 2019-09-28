cac00416e5ab342fe3a6e9213a9773bcb12943ed
Toshinori Tsugita <tsugita.dev@gmail.com>
2019-09-28 23:11:30 +0900

    Solve abc142 a~d

---
+++ contests/abc142/abc142_d/main.go
@@ -0,0 +1,438 @@
+// https://atcoder.jp/contests/abc142/tasks/abc142_d
+
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+	"reflect"
+	"runtime"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+// d: debug IO. it can print debug in test.
+func solve(io *Io, d *Io) {
+	A := io.NextInt()
+	B := io.NextInt()
+
+	// 公約数のペアに含まれる素数と 1
+
+	max := Max(A, B)
+	min := Min(A, B)
+	nums := []int{}
+
+	for num := 1; num*num <= max; num++ {
+		if max%num != 0 {
+			continue
+		}
+
+		if min%num == 0 {
+			nums = append(nums, num)
+		}
+
+		other := max / num
+
+		if num == other {
+			continue
+		}
+
+		if min%other == 0 {
+			nums = append(nums, other)
+		}
+	}
+
+	sort.Ints(nums)
+
+	res := 0
+
+	for i, num := range nums {
+		prime := true
+
+		for j := 0; j < i; j++ {
+			other := nums[j]
+
+			if other == 1 {
+				continue
+			}
+
+			if num%other == 0 {
+				prime = false
+				break
+			}
+		}
+
+		if prime {
+			res++
+		}
+	}
+
+	io.Println(res)
+}
+
+func gcd(a, b int) int {
+	if b == 0 {
+		return a
+	}
+
+	return gcd(b, a%b)
+}
+
+// GCD の素因数の個数 + 1
+func solve2(io *Io, d *Io) {
+	A := io.NextInt()
+	B := io.NextInt()
+
+	g := gcd(A, B)
+
+	factorCands := []int{}
+
+	for c := 1; c*c <= g; c++ {
+		if g%c != 0 {
+			continue
+		}
+
+		factorCands = append(factorCands, c)
+
+		if c != g/c {
+			factorCands = append(factorCands, g/c)
+		}
+	}
+
+	sort.Ints(factorCands)
+
+	res := 0
+
+	for _, f := range factorCands {
+		if g%f != 0 {
+			continue
+		}
+
+		res++
+
+		if f == 1 {
+			continue
+		}
+
+		for g%f == 0 {
+			g /= f
+		}
+	}
+
+	io.Println(res)
+}
+
+func main() {
+	io := NewIo()
+	defer io.Flush()
+	solve2(io, nil)
+}
+
+/* IO Helpers */
+
+// Io combines reader, writer, & tokens as the state when processing the input
+type Io struct {
+	reader    *bufio.Reader
+	writer    *bufio.Writer
+	tokens    []string
+	nextToken int
+}
+
+// NewIo creates Io with Stdin & Stdout
+func NewIo() *Io {
+	return &Io{
+		reader: bufio.NewReader(os.Stdin),
+		writer: bufio.NewWriter(os.Stdout),
+	}
+}
+
+// Flush calls the writer's Flush
+func (io *Io) Flush() {
+	io.writer.Flush()
+}
+
+// NextLine returns the string from reader.ReadLine()
+func (io *Io) NextLine() string {
+	if io.nextToken < len(io.tokens) {
+		panic("io.nextToken < len(io.tokens)")
+	}
+
+	var buffer bytes.Buffer
+
+	for {
+		line, isPrefix, err := io.reader.ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		buffer.Write(line)
+		if !isPrefix {
+			break
+		}
+	}
+
+	return buffer.String()
+}
+
+// NextLines returns []string from next n lines
+func (io *Io) NextLines(n int) []string {
+	res := make([]string, n)
+	for i := 0; i < n; i++ {
+		res[i] = io.NextLine()
+	}
+	return res
+}
+
+// Next returns the string token (partial string of the line divided by spaces)
+func (io *Io) Next() string {
+	if io.nextToken >= len(io.tokens) {
+		line := io.NextLine()
+		io.tokens = strings.Fields(line)
+		io.nextToken = 0
+	}
+
+	res := io.tokens[io.nextToken]
+	io.nextToken++
+	return res
+}
+
+// NextStrings returns the []string from the next n tokens
+func (io *Io) NextStrings(n int) []string {
+	res := make([]string, n)
+	for i := 0; i < n; i++ {
+		res[i] = io.Next()
+	}
+	return res
+}
+
+// NextRunes returns next string as []rune
+func (io *Io) NextRunes() []rune {
+	return []rune(io.Next())
+}
+
+// NextInt returns the int from the next token
+func (io *Io) NextInt() int {
+	res, err := strconv.Atoi(io.Next())
+	if err != nil {
+		panic(err)
+	}
+	return res
+}
+
+// NextInts returns the []int from the next n tokens
+func (io *Io) NextInts(n int) []int {
+	res := make([]int, n)
+	for i := 0; i < n; i++ {
+		res[i] = io.NextInt()
+	}
+	return res
+}
+
+// NextFloat returns the float64 from the next token
+func (io *Io) NextFloat() float64 {
+	res, err := strconv.ParseFloat(io.Next(), 64)
+	if err != nil {
+		panic(err)
+	}
+	return res
+}
+
+// NextFloats returns the []float64 from the next n tokens
+func (io *Io) NextFloats(n int) []float64 {
+	res := make([]float64, n)
+	for i := 0; i < n; i++ {
+		res[i] = io.NextFloat()
+	}
+	return res
+}
+
+// Println prints the input to the writer in an easy-to-see form
+func (io *Io) Println(a ...interface{}) {
+	var values []string
+	for i := 0; i < len(a); i++ {
+		values = append(values, "%v")
+	}
+	io.Printfln(strings.Join(values, " "), a...)
+}
+
+// Print calls Fprint to the writer
+func (io *Io) Print(a interface{}) {
+	fmt.Fprint(io.writer, a)
+}
+
+// Printf calls Fprintf to the writer
+func (io *Io) Printf(format string, a ...interface{}) {
+	fmt.Fprintf(io.writer, format, a...)
+}
+
+// Printfln calls Fprint to the writer
+func (io *Io) Printfln(format string, a ...interface{}) {
+	fmt.Fprintf(io.writer, format+"\n", a...)
+}
+
+// PrintInts prints ints with space and new line at the end
+func (io *Io) PrintInts(ints []int) {
+	for i, e := range ints {
+		io.Print(e)
+
+		if i == len(ints)-1 {
+			io.Println()
+		} else {
+			io.Print(" ")
+		}
+	}
+}
+
+// Debug calls Println and Flush immediately
+func (io *Io) Debug(a ...interface{}) {
+	if io == nil {
+		return
+	}
+
+	io.Println(a...)
+	io.Flush()
+}
+
+// Assert checks the value equality for debug
+func (io *Io) Assert(a, b interface{}) {
+	if io == nil {
+		return
+	}
+
+	if !reflect.DeepEqual(a, b) {
+		_, _, line, _ := runtime.Caller(1)
+		io.Debug("Diff:", line, "\na:", a, "\nb:", b)
+	}
+}
+
+/* Math Helpers */
+
+// MOD is 10^9 + 7
+const MOD = 1000000007
+
+// Max of the ints
+func Max(numbers ...int) int {
+	max := numbers[0]
+	for i, number := range numbers {
+		if i == 0 {
+			continue
+		}
+		if number > max {
+			max = number
+		}
+	}
+	return max
+}
+
+// Min of the ints
+func Min(numbers ...int) int {
+	min := numbers[0]
+	for i, number := range numbers {
+		if i == 0 {
+			continue
+		}
+		if min > number {
+			min = number
+		}
+	}
+	return min
+}
+
+// Sum of the ints
+func Sum(numbers ...int) int {
+	sum := 0
+
+	for _, number := range numbers {
+		sum += number
+	}
+
+	return sum
+}
+
+// Pow calculates the pow for int with O(log e)
+func Pow(a, e int) int {
+	if a < 0 || e < 0 {
+		panic(fmt.Sprintf("Pow was called for a < 0 or e < 0, a: %d, e: %d", a, e))
+	}
+
+	if e == 0 {
+		return 1
+	}
+
+	if e%2 == 0 {
+		half := Pow(a, e/2)
+		return half * half
+	}
+
+	return a * Pow(a, e-1)
+}
+
+// Abs returns the absolute value of the input
+func Abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+
+	return a
+}
+
+/* Structure Helpers */
+
+// Set is orderless List
+type Set map[interface{}]struct{}
+
+// NewSet returns empty Set
+func NewSet() *Set {
+	return &Set{}
+}
+
+// Add the value to the set
+func (set *Set) Add(value interface{}) {
+	(*set)[value] = struct{}{}
+}
+
+// Includes check the set has the value
+func (set *Set) Includes(value interface{}) bool {
+	_, included := (*set)[value]
+	return included
+}
+
+// Remove deletes the value from the set if exists
+func (set *Set) Remove(value interface{}) {
+	if !set.Includes(value) {
+		return
+	}
+
+	delete(*set, value)
+}
+
+// ImmutableSort returns sorted slice without mutating original one
+func ImmutableSort(slice []int) []int {
+	res := make([]int, len(slice))
+	copy(res, slice)
+	sort.Ints(res)
+	return res
+}
+
+/* Util helpers */
+
+// Ternary is like `cond ? a : b`. should assert the returned value like `Ternary(cond, a, b).(int)`
+func Ternary(cond bool, a, b interface{}) interface{} {
+	if cond {
+		return a
+	}
+
+	return b
+}
+
+// TernaryInt uses Ternary and assert the value as int
+func TernaryInt(cond bool, a, b int) int {
+	return Ternary(cond, a, b).(int)
+}
+
+// TernaryString uses Ternary and assert the value as string
+func TernaryString(cond bool, a, b string) string {
+	return Ternary(cond, a, b).(string)
+}
