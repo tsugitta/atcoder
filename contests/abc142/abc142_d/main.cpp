ef2a4d78181c3ad790bd92ebfee4e39cc6ae4ed5
Toshinori Tsugita <tsugita.dev@gmail.com>
2019-09-30 14:18:51 +0900

    Solve abc142 d (c++)

--- contests/abc142/abc142_d/main.cpp
+++ contests/abc142/abc142_d/main.cpp
@@ -1,4 +1,4 @@
-// {{ .URL }}
+// https://atcoder.jp/contests/abc142/tasks/abc142_d
 
 #include "algorithm"
 #include "iostream"
@@ -12,7 +12,38 @@ inline bool chmax(T& a, T b);
 template <class T>
 inline bool chmin(T& a, T b);
 
+vector<pair<ll, ll>> factorize(ll n) {
+  vector<pair<ll, ll>> res;
+
+  for (ll i = 2; i * i <= n; i++) {
+    if (n % i != 0) continue;
+
+    auto r = pair<ll, ll>(i, 0);
+
+    while (n % i == 0) {
+      n /= i;
+      r.second++;
+    }
+
+    res.push_back(r);
+  }
+
+  // sqrt(n) 以上の素因数は高々一つ
+  if (n != 1) res.emplace_back(n, 1);
+
+  return res;
+}
+
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+
 void solve() {
+  ll A, B;
+  cin >> A >> B;
+
+  ll g = gcd(A, B);
+  auto f = factorize(g);
+  ll res = f.size() + 1;
+  cout << res << endl;
 }
 
 #ifndef TEST
