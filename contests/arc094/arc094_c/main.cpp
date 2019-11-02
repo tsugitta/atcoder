c2413c864e0d164516604298747128e7fdb3b4db
Toshinori Tsugita <tsugita.dev@gmail.com>
2019-11-02 22:38:23 +0900

    Solve arc094 c

---
+++ contests/arc094/arc094_c/main.cpp
@@ -0,0 +1,105 @@
+// https://atcoder.jp/contests/arc094/tasks/arc094_c
+
+#include "algorithm"
+#include "iostream"
+#include "numeric"
+#include "queue"
+#include "set"
+#include "vector"
+#define rep(i, to) for (ll i = 0; i < (to); ++i)
+#define repf(i, from, to) for (ll i = from; i < (to); ++i)
+using namespace std;
+typedef long long ll;
+template <typename T>
+using V = vector<T>;
+using VL = V<ll>;
+using VVL = V<VL>;
+template <typename T, typename U>
+using P = pair<T, U>;
+using PL = P<ll, ll>;
+using VPL = V<PL>;
+template <typename T>
+inline bool chmax(T& a, T b);
+template <typename T>
+inline bool chmin(T& a, T b);
+void print_ints(vector<ll> v);
+template <typename T>
+void drop(T a);
+
+void solve() {
+  ll N;
+  cin >> N;
+  VL as(N);
+  VL bs(N);
+
+  ll last_element = 1e18;  // a > b なる b で最小のもの。操作で最後に一致させる
+
+  ll sum = 0;
+  rep(i, N) {
+    cin >> as[i] >> bs[i];
+    sum += as[i];
+
+    if (as[i] > bs[i]) chmin(last_element, bs[i]);
+  }
+
+  if (as == bs) drop(0);
+
+  ll a_sum = accumulate(as.begin(), as.end(), 0ll);
+  ll res = a_sum - last_element;
+
+  drop(res);
+}
+
+struct exit_exception : public std::exception {
+  const char* what() const throw() { return "Exited"; }
+};
+
+#ifndef TEST
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  try {
+    solve();
+  } catch (exit_exception& e) {
+  }
+
+  return 0;
+}
+#endif
+
+template <typename T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+void print_ints(vector<ll> v) {
+  rep(i, v.size()) {
+    if (i > 0) {
+      cout << " ";
+    }
+
+    cout << v[i];
+  }
+
+  cout << endl;
+}
+
+template <typename T>
+void drop(T res) {
+  cout << res << endl;
+  throw exit_exception();
+}
