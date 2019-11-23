3388fd12d72c0f166d8c292f1abdc872bab746d8
Toshinori Tsugita <tsugita.dev@gmail.com>
2019-11-24 01:40:57 +0900

    Solve arc091 a, b

---
+++ contests/arc091/arc091_b/main.cpp
@@ -0,0 +1,109 @@
+// https://atcoder.jp/contests/arc091/tasks/arc091_b
+
+#include "algorithm"
+#include "cmath"
+#include "functional"
+#include "iostream"
+#include "numeric"
+#include "queue"
+#include "set"
+#include "vector"
+#define rep(i, to) for (ll i = 0; i < (to); ++i)
+#define repf(i, from, to) for (ll i = from; i < (to); ++i)
+#define repr(i, from) for (ll i = from - 1; i >= 0; --i)
+#define all(vec) vec.begin(), vec.end()
+#define fi first
+#define se second
+using namespace std;
+typedef long long ll;
+template <typename T>
+using V = vector<T>;
+using VL = V<ll>;
+using VVL = V<VL>;
+template <typename T, typename U>
+using P = pair<T, U>;
+using PL = P<ll, ll>;
+using VPL = V<PL>;
+template <typename T>
+inline bool chmax(T& a, T b);
+template <typename T>
+inline bool chmin(T& a, T b);
+void print_ints(vector<ll> v);
+template <typename T>
+void drop(T a);
+
+const ll INF = 1e18;
+
+void solve() {
+  ll N, K;
+  cin >> N >> K;
+
+  ll res = 0;
+
+  rep(b, N + 1) {
+    if (b - 1 < K) continue;
+    ll round_down = N / b;
+    // round_down 回 0 ~ b-1 の余りが一度ずつ現れている
+    res += max(0ll, round_down * (b - 1 - (K - 1)));
+
+    // 残りの 1, 2.. rest について K 以上のものをカウント
+    ll rest = N - round_down * b;
+    res += max(0ll, rest - max(0ll, (K - 1)));
+  }
+
+  cout << res << endl;
+}
+
+struct exit_exception : public std::exception {
+  const char* what() const throw() { return "Exited"; }
+};
+
+#ifndef TEST
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  try {
+    solve();
+  } catch (exit_exception& e) {
+  }
+
+  return 0;
+}
+#endif
+
+template <typename T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+void print_ints(vector<ll> v) {
+  rep(i, v.size()) {
+    if (i > 0) {
+      cout << " ";
+    }
+
+    cout << v[i];
+  }
+
+  cout << endl;
+}
+
+template <typename T>
+void drop(T res) {
+  cout << res << endl;
+  throw exit_exception();
+}
