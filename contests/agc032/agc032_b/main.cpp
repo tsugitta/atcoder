dce0030d6354e834b5d11e7faadf1996bd715705
Toshinori Tsugita <tsugita.dev@gmail.com>
2020-01-02 02:02:31 +0900

    Solve agc032 b

---
+++ contests/agc032/agc032_b/main.cpp
@@ -0,0 +1,132 @@
+// https://atcoder.jp/contests/agc032/tasks/agc032_b
+
+#include "algorithm"
+#include "bitset"
+#include "cmath"
+#include "functional"
+#include "iomanip"
+#include "iostream"
+#include "map"
+#include "numeric"
+#include "queue"
+#include "set"
+#include "string"
+#include "vector"
+#define rep(i, to) for (ll i = 0; i < (to); ++i)
+#define rep1(i, to) for (ll i = 1; i <= (to); ++i)
+#define repf(i, from, to) for (ll i = from; i < (to); ++i)
+#define repr(i, from) for (ll i = from - 1; i >= 0; --i)
+#define all(vec) vec.begin(), vec.end()
+#define unless(cond) if (!(cond))
+#define fi first
+#define se second
+using namespace std;
+typedef long long ll;
+template <typename T>
+using V = vector<T>;
+using VL = V<ll>;
+using VVL = V<VL>;
+template <typename T, typename U>
+using P = pair<T, U>;
+using PL = P<ll, ll>;
+using VPL = V<PL>;
+template <typename T>
+inline bool chmax(T& a, T b);
+template <typename T>
+inline bool chmin(T& a, T b);
+void print_ints(vector<ll> v);
+template <typename T>
+void drop(T a);
+
+const ll INF = 1e18;
+
+void solve() {
+  ll N;
+  cin >> N;
+
+  VVL edge_list(N, VL(0));
+
+  ll n = N;
+
+  if (N % 2 == 1) {
+    rep(i, N - 1) {
+      edge_list[i].push_back(N - 1);
+      edge_list[N - 1].push_back(i);
+    }
+
+    n--;
+  }
+
+  rep(j, n) rep(i, j) {
+    if (j == n - 1 - i) continue;
+    edge_list[i].push_back(j);
+    edge_list[j].push_back(i);
+  }
+
+  ll edge_ct = 0;
+  rep(i, N) edge_ct += edge_list[i].size();
+  edge_ct /= 2;
+
+  cout << edge_ct << endl;
+
+  rep(u, N) {
+    for (ll v : edge_list[u]) {
+      unless(u < v) continue;
+      cout << u + 1 << " " << v + 1 << endl;
+    }
+  }
+}
+
+struct exit_exception : public std::exception {
+  const char* what() const throw() { return "Exited"; }
+};
+
+#ifndef TEST
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  try {
+    solve();
+  } catch (exit_exception& e) {
+  }
+
+  return 0;
+}
+#endif
+
+template <typename T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+void print_ints(vector<ll> v) {
+  rep(i, v.size()) {
+    if (i > 0) {
+      cout << " ";
+    }
+
+    cout << v[i];
+  }
+
+  cout << endl;
+}
+
+template <typename T>
+void drop(T res) {
+  cout << res << endl;
+  throw exit_exception();
+}
