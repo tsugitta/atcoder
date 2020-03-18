277598d60fc0adce1b41e2e67e3dd8e1bf06a254
Toshinori Tsugita <tsugita.dev@gmail.com>
2020-03-18 18:29:28 +0900

    Solve apc001 c

---
+++ contests/apc001/apc001_c/main.cpp
@@ -0,0 +1,137 @@
+// https://atcoder.jp/contests/apc001/tasks/apc001_c
+
+#include "algorithm"
+#include "bitset"
+#include "cmath"
+#include "functional"
+#include "iomanip"
+#include "iostream"
+#include "map"
+#include "numeric"
+#include "queue"
+#include "set"
+#include "string"
+#include "vector"
+#define rep(i, to) for (ll i = 0; i < (to); ++i)
+#define rep1(i, to) for (ll i = 1; i <= (to); ++i)
+#define repf(i, from, to) for (ll i = from; i < (to); ++i)
+#define repr(i, from) for (ll i = from - 1; i >= 0; --i)
+#define all(vec) vec.begin(), vec.end()
+#define unless(cond) if (!(cond))
+#define fi first
+#define se second
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+template <typename T>
+using V = vector<T>;
+using VL = V<ll>;
+using VVL = V<VL>;
+template <typename T, typename U>
+using P = pair<T, U>;
+using PL = P<ll, ll>;
+using VPL = V<PL>;
+template <typename T>
+inline bool chmax(T& a, T b);
+template <typename T>
+inline bool chmin(T& a, T b);
+void print_ints(vector<ll> v);
+template <typename T>
+void drop(T a);
+
+const ll INF = 1e18;
+
+void solve() {
+  ll N;
+  cin >> N;
+
+  string Vacant = "Vacant";
+  string Male = "Male";
+  string Female = "Female";
+
+  ll left_i = 0;
+  cout << left_i << endl;
+
+  string left;
+  cin >> left;
+  if (left == Vacant) return;
+
+  ll right_i = N - 1;
+  cout << right_i << endl;
+
+  string right;
+  cin >> right;
+  if (right == Vacant) return;
+
+  while (true) {
+    ll mid_i = (left_i + right_i) / 2;
+    cout << mid_i << endl;
+
+    string mid;
+    cin >> mid;
+    if (mid == Vacant) return;
+
+    if ((mid_i - left_i) % 2 == 0 && left != mid ||
+        (mid_i - left_i) % 2 == 1 && left == mid) {
+      right_i = mid_i;
+      right = mid;
+    } else {
+      left_i = mid_i;
+      left = mid;
+    }
+  }
+}
+
+struct exit_exception : public std::exception {
+  const char* what() const throw() { return "Exited"; }
+};
+
+#ifndef TEST
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  try {
+    solve();
+  } catch (exit_exception& e) {
+  }
+
+  return 0;
+}
+#endif
+
+template <typename T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+void print_ints(vector<ll> v) {
+  rep(i, v.size()) {
+    if (i > 0) {
+      cout << " ";
+    }
+
+    cout << v[i];
+  }
+
+  cout << endl;
+}
+
+template <typename T>
+void drop(T res) {
+  cout << res << endl;
+  throw exit_exception();
+}
