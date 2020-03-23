873cd6572fedf4f7fe5e8191d12ccb762ca19535
Toshinori Tsugita <tsugita.dev@gmail.com>
2020-03-23 23:49:01 +0900

    Solve abc159 f

---
+++ contests/abc159/abc159_f/main.cpp
@@ -0,0 +1,184 @@
+// https://atcoder.jp/contests/abc159/tasks/abc159_f
+
+#include "algorithm"
+#include "bitset"
+#include "cmath"
+#include "functional"
+#include "iomanip"
+#include "iostream"
+#include "map"
+#include "numeric"
+#include "queue"
+#include "set"
+#include "string"
+#include "vector"
+#define rep(i, to) for (ll i = 0; i < (to); ++i)
+#define rep1(i, to) for (ll i = 1; i <= (to); ++i)
+#define repf(i, from, to) for (ll i = from; i < (to); ++i)
+#define repr(i, from) for (ll i = from - 1; i >= 0; --i)
+#define all(vec) vec.begin(), vec.end()
+#define unless(cond) if (!(cond))
+#define fi first
+#define se second
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+template <typename T>
+using V = vector<T>;
+using VL = V<ll>;
+using VVL = V<VL>;
+template <typename T, typename U>
+using P = pair<T, U>;
+using PL = P<ll, ll>;
+using VPL = V<PL>;
+template <typename T>
+inline bool chmax(T& a, T b);
+template <typename T>
+inline bool chmin(T& a, T b);
+void print_ints(vector<ll> v);
+template <typename T>
+void drop(T a);
+
+const ll INF = 1e18;
+template <int MOD>
+class Fp {
+ public:
+  long long val;
+  constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
+    if (val < 0) val += MOD;
+  }
+  constexpr int getmod() { return MOD; }
+  constexpr Fp operator-() const noexcept { return val ? MOD - val : 0; }
+  constexpr Fp operator+(const Fp& r) const noexcept { return Fp(*this) += r; }
+  constexpr Fp operator-(const Fp& r) const noexcept { return Fp(*this) -= r; }
+  constexpr Fp operator*(const Fp& r) const noexcept { return Fp(*this) *= r; }
+  constexpr Fp operator/(const Fp& r) const noexcept { return Fp(*this) /= r; }
+  constexpr Fp& operator+=(const Fp& r) noexcept {
+    val += r.val;
+    if (val >= MOD) val -= MOD;
+    return *this;
+  }
+  constexpr Fp& operator-=(const Fp& r) noexcept {
+    val -= r.val;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr Fp& operator*=(const Fp& r) noexcept {
+    val = val * r.val % MOD;
+    return *this;
+  }
+  constexpr Fp& operator/=(const Fp& r) noexcept {
+    long long a = r.val, b = MOD, u = 1, v = 0;
+    while (b) {
+      long long t = a / b;
+      a -= t * b;
+      swap(a, b);
+      u -= t * v;
+      swap(u, v);
+    }
+    val = val * u % MOD;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr bool operator==(const Fp& r) const noexcept {
+    return this->val == r.val;
+  }
+  constexpr bool operator!=(const Fp& r) const noexcept {
+    return this->val != r.val;
+  }
+  friend constexpr ostream& operator<<(ostream& os, const Fp<MOD>& x) noexcept {
+    return os << x.val;
+  }
+  friend constexpr Fp<MOD> modpow(const Fp<MOD>& a, long long n) noexcept {
+    if (n == 0) return 1;
+    auto t = modpow(a, n / 2);
+    t = t * t;
+    if (n & 1) t = t * a;
+    return t;
+  }
+};
+
+const ll MOD = 998244353;
+using mint = Fp<MOD>;
+
+void solve() {
+  ll N, S;
+  cin >> N >> S;
+
+  VL as(N);
+  rep(i, N) cin >> as[i];
+
+  // dp[i][j][k]: i 番目の要素までで和が j となる通り。 k: 0, 1, 2 順に
+  // 左端未定, 左端確定, 右端確定
+  V<V<V<mint>>> dp(N + 1, V<V<mint>>(S + 1, V<mint>(3)));
+
+  dp[0][0][0] = 1;
+
+  rep(i, N) rep(j, S + 1) {
+    dp[i + 1][j][0] += dp[i][j][0];
+    dp[i + 1][j][1] += dp[i][j][0] + dp[i][j][1];
+    dp[i + 1][j][2] += dp[i][j][0] + dp[i][j][1] + dp[i][j][2];
+
+    if (j + as[i] <= S) {
+      dp[i + 1][j + as[i]][1] += dp[i][j][0] + dp[i][j][1];
+      dp[i + 1][j + as[i]][2] += dp[i][j][0] + dp[i][j][1];
+    }
+  }
+
+  mint res = dp[N][S][2];
+  cout << res << endl;
+}
+
+struct exit_exception : public std::exception {
+  const char* what() const throw() { return "Exited"; }
+};
+
+#ifndef TEST
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  try {
+    solve();
+  } catch (exit_exception& e) {
+  }
+
+  return 0;
+}
+#endif
+
+template <typename T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+void print_ints(vector<ll> v) {
+  rep(i, v.size()) {
+    if (i > 0) {
+      cout << " ";
+    }
+
+    cout << v[i];
+  }
+
+  cout << endl;
+}
+
+template <typename T>
+void drop(T res) {
+  cout << res << endl;
+  throw exit_exception();
+}
