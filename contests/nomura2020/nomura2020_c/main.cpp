88b7f3e1c5303cf8f53b34581d3df09286aebde3
Toshinori Tsugita <tsugita.dev@gmail.com>
2020-07-18 02:23:18 +0900

    Solve nomura2020 c

---
+++ contests/nomura2020/nomura2020_c/main.cpp
@@ -0,0 +1,113 @@
+// https://atcoder.jp/contests/nomura2020/tasks/nomura2020_c
+
+#include "algorithm"
+#include "bitset"
+#include "cmath"
+#include "functional"
+#include "iomanip"
+#include "iostream"
+#include "map"
+#include "numeric"
+#include "queue"
+#include "set"
+#include "string"
+#include "vector"
+#define rep(i, to) for (ll i = 0; i < (to); ++i)
+#define rep1(i, to) for (ll i = 1; i <= (to); ++i)
+#define repf(i, from, to) for (ll i = from; i < (to); ++i)
+#define repr(i, from) for (ll i = from - 1; i >= 0; --i)
+#define all(vec) vec.begin(), vec.end()
+#define unless(cond) if (!(cond))
+#define fi first
+#define se second
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+template <typename T>
+using V = vector<T>;
+using VL = V<ll>;
+using VVL = V<VL>;
+template <typename T, typename U>
+using P = pair<T, U>;
+using PL = P<ll, ll>;
+using VPL = V<PL>;
+
+template <typename T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline ll len(V<T> arr) {
+  return arr.size();
+}
+
+struct exit_exception : public std::exception {
+  const char* what() const throw() { return "Exited"; }
+};
+
+template <typename T>
+void drop(T res) {
+  cout << res << endl;
+  throw exit_exception();
+}
+
+const ll INF = 1e18;
+
+void solve();
+
+#ifndef TEST
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  try {
+    solve();
+  } catch (exit_exception& e) {
+  }
+
+  return 0;
+}
+#endif
+
+void solve() {
+  ll N;
+  cin >> N;
+
+  VL as(N + 1);
+  rep(i, N + 1) cin >> as[i];
+
+  VL rev_acc(N + 1);
+  repr(i, N + 1) { rev_acc[i] = as[i] + (i < N ? rev_acc[i + 1] : 0); }
+
+  if (N == 0) {
+    ll res = as[0] == 1 ? 1 : -1;
+    drop(res);
+  }
+
+  ll prev_non_leaf_ct = 1 - as[0];
+  ll res = 1;
+
+  rep1(i, N) {
+    ll ma = min(2 * prev_non_leaf_ct, rev_acc[i]);
+    unless(as[i] <= ma) drop(-1);
+
+    res += ma;
+    prev_non_leaf_ct = ma - as[i];
+  }
+
+  cout << res << "\n";
+}
