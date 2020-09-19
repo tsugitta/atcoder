ffe1126004a02c162fb3fa888f5d52c9db1ce46e
Toshinori Tsugita <tsugita.dev@gmail.com>
2020-09-20 02:16:18 +0900

    Solve abc179

---
+++ contests/abc179/abc179_d/main.cpp
@@ -0,0 +1,233 @@
+// https://atcoder.jp/contests/abc179/tasks/abc179_d
+
+#include "algorithm"
+#include "bitset"
+#include "cmath"
+#include "functional"
+#include "iomanip"
+#include "iostream"
+#include "map"
+#include "numeric"
+#include "queue"
+#include "set"
+#include "string"
+#include "vector"
+#define rep(i, to) for (ll i = 0; i < (to); ++i)
+#define rep1(i, to) for (ll i = 1; i <= (to); ++i)
+#define repf(i, from, to) for (ll i = from; i < (to); ++i)
+#define repr(i, from) for (ll i = from - 1; i >= 0; --i)
+#define all(vec) vec.begin(), vec.end()
+#define unless(cond) if (!(cond))
+#define fi first
+#define se second
+using namespace std;
+using ll = long long;
+using ld = long double;
+template <typename T>
+using V = vector<T>;
+using VL = V<ll>;
+template <typename T>
+using VV = V<V<T>>;
+using VVL = VV<ll>;
+template <typename T>
+using VVV = VV<V<T>>;
+template <typename T, typename U>
+using P = pair<T, U>;
+using PL = P<ll, ll>;
+using VPL = V<PL>;
+template <typename T>
+using asc_pq = priority_queue<T, V<T>, greater<T>>;
+
+template <typename T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline ll len(V<T> arr) {
+  return arr.size();
+}
+
+struct exit_exception : public std::exception {
+  const char* what() const throw() { return "Exited"; }
+};
+
+template <typename T>
+void drop(T res) {
+  cout << res << endl;
+  throw exit_exception();
+}
+
+const ll INF = 1e18;
+
+void solve();
+
+#ifndef TEST
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  try {
+    solve();
+  } catch (exit_exception& e) {
+  }
+
+  return 0;
+}
+#endif
+
+template <int MOD>
+class Fp {
+ public:
+  long long val;
+  constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
+    if (val < 0) val += MOD;
+  }
+  constexpr int getmod() { return MOD; }
+  constexpr Fp operator-() const noexcept { return val ? MOD - val : 0; }
+  constexpr Fp operator+(const Fp& r) const noexcept { return Fp(*this) += r; }
+  constexpr Fp operator-(const Fp& r) const noexcept { return Fp(*this) -= r; }
+  constexpr Fp operator*(const Fp& r) const noexcept { return Fp(*this) *= r; }
+  constexpr Fp operator/(const Fp& r) const noexcept { return Fp(*this) /= r; }
+  constexpr Fp& operator+=(const Fp& r) noexcept {
+    val += r.val;
+    if (val >= MOD) val -= MOD;
+    return *this;
+  }
+  constexpr Fp& operator-=(const Fp& r) noexcept {
+    val -= r.val;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr Fp& operator*=(const Fp& r) noexcept {
+    val = val * r.val % MOD;
+    return *this;
+  }
+  constexpr Fp& operator/=(const Fp& r) noexcept {
+    long long a = r.val, b = MOD, u = 1, v = 0;
+    while (b) {
+      long long t = a / b;
+      a -= t * b;
+      swap(a, b);
+      u -= t * v;
+      swap(u, v);
+    }
+    val = val * u % MOD;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr bool operator==(const Fp& r) const noexcept {
+    return this->val == r.val;
+  }
+  constexpr bool operator!=(const Fp& r) const noexcept {
+    return this->val != r.val;
+  }
+  friend constexpr ostream& operator<<(ostream& os, const Fp<MOD>& x) noexcept {
+    return os << x.val;
+  }
+  friend constexpr Fp<MOD> modpow(const Fp<MOD>& a, long long n) noexcept {
+    if (n == 0) return 1;
+    auto t = modpow(a, n / 2);
+    t = t * t;
+    if (n & 1) t = t * a;
+    return t;
+  }
+};
+
+const ll MOD = 998244353;
+// const ll MOD = 1000000007;
+using mint = Fp<MOD>;
+
+struct combination_table {
+  // i! % MOD M
+  V<ll> fact;
+  // (i!)^(-1) MOD M
+  V<ll> finv;
+  // i^(-1) MOD M
+  V<ll> inv;
+};
+
+combination_table ct;
+
+// ref: http://drken1215.hatenablog.com/entry/2018/06/08/210000
+void prepare_combination(ll max) {
+  V<ll> fact(max + 1);
+  V<ll> finv(max + 1);
+  V<ll> inv(max + 1);
+
+  fact[0] = 1;
+  fact[1] = 1;
+  finv[0] = 1;
+  finv[1] = 1;
+  inv[1] = 1;
+
+  for (ll i = 2; i <= max; i++) {
+    fact[i] = fact[i - 1] * i % MOD;
+    inv[i] = MOD - inv[MOD % i] * (MOD / i) %
+                       MOD;  // a^(-1) ≡ -(p % a)^(-1) * (p/a) MOD p
+    finv[i] = finv[i - 1] * inv[i] % MOD;
+  }
+
+  ct = {
+      fact,
+      finv,
+      inv,
+  };
+}
+
+mint c_m(ll n, ll k) {
+  if (n < k) return 0;
+  if (n < 0 || k < 0) return 0;
+  return mint(ct.fact[n]) * ct.finv[k] * ct.finv[n - k];
+}
+
+// 重複組合せ n 種類から k 個重複を許して取る通り
+mint h_m(ll n, ll k) { return c_m((n - 1) + k, k); }
+
+mint p_m(ll n, ll k) { return c_m(n, k) * ct.fact[k]; }
+
+void solve() {
+  ll N, K;
+  cin >> N >> K;
+
+  VPL ranges(K);
+
+  rep(i, K) cin >> ranges[i].fi >> ranges[i].se;
+
+  V<mint> cts(N);
+  V<mint> acc(N + 1);
+
+  cts[0] = 1;
+  acc[1] = 1;
+
+  rep1(i, N - 1) {
+    mint r = 0;
+
+    for (auto range : ranges) {
+      // [left, right) から飛んでくるとする
+      ll left = max(0ll, i - range.se);
+      ll right = i - range.fi + 1;
+
+      if (right <= 0) continue;
+      r += acc[right] - acc[left];
+    }
+
+    cts[i] = r;
+    acc[i + 1] = acc[i] + r;
+  }
+
+  cout << cts[N - 1] << "\n";
+}
---
+++ contests/abc179/abc179_f/main.cpp
@@ -0,0 +1,211 @@
+// https://atcoder.jp/contests/abc179/tasks/abc179_f
+
+#include "algorithm"
+#include "bitset"
+#include "cmath"
+#include "functional"
+#include "iomanip"
+#include "iostream"
+#include "map"
+#include "numeric"
+#include "queue"
+#include "set"
+#include "string"
+#include "vector"
+#define rep(i, to) for (ll i = 0; i < (to); ++i)
+#define rep1(i, to) for (ll i = 1; i <= (to); ++i)
+#define repf(i, from, to) for (ll i = from; i < (to); ++i)
+#define repr(i, from) for (ll i = from - 1; i >= 0; --i)
+#define all(vec) vec.begin(), vec.end()
+#define unless(cond) if (!(cond))
+#define fi first
+#define se second
+using namespace std;
+using ll = long long;
+using ld = long double;
+template <typename T>
+using V = vector<T>;
+using VL = V<ll>;
+template <typename T>
+using VV = V<V<T>>;
+using VVL = VV<ll>;
+template <typename T>
+using VVV = VV<V<T>>;
+template <typename T, typename U>
+using P = pair<T, U>;
+using PL = P<ll, ll>;
+using VPL = V<PL>;
+template <typename T>
+using asc_pq = priority_queue<T, V<T>, greater<T>>;
+
+template <typename T>
+inline bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline ll len(V<T> arr) {
+  return arr.size();
+}
+
+struct exit_exception : public std::exception {
+  const char *what() const throw() { return "Exited"; }
+};
+
+template <typename T>
+void drop(T res) {
+  cout << res << endl;
+  throw exit_exception();
+}
+
+const ll INF = 1e18;
+
+void solve();
+
+#ifndef TEST
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  try {
+    solve();
+  } catch (exit_exception &e) {
+  }
+
+  return 0;
+}
+#endif
+
+template <class Monoid, class Action>
+struct LazySegTree {
+  using FuncMonoid = function<Monoid(Monoid, Monoid)>;
+  using FuncAction = function<void(Monoid &, Action)>;
+  using FuncLazy = function<void(Action &, Action)>;
+  FuncMonoid FM;
+  FuncAction FA;
+  FuncLazy FL;
+  Monoid UNITY_MONOID;
+  Action UNITY_LAZY;
+  ll SIZE, HEIGHT;
+  vector<Monoid> dat;
+  vector<Action> lazy;
+
+  LazySegTree(ll n, const FuncMonoid fm, const FuncAction fa, const FuncLazy fl,
+              const Monoid &unity_monoid, const Action &unity_lazy)
+      : FM(fm),
+        FA(fa),
+        FL(fl),
+        UNITY_MONOID(unity_monoid),
+        UNITY_LAZY(unity_lazy) {
+    SIZE = 1;
+    HEIGHT = 0;
+    while (SIZE < n) SIZE <<= 1, ++HEIGHT;
+    dat.assign(SIZE * 2, UNITY_MONOID);
+    lazy.assign(SIZE * 2, UNITY_LAZY);
+  }
+
+  void init(ll n, const FuncMonoid fm, const FuncAction fa, const FuncLazy fl,
+            const Monoid &unity_monoid, const Action &unity_lazy) {
+    FM = fm;
+    FA = fa;
+    FL = fl;
+    UNITY_MONOID = unity_monoid;
+    UNITY_LAZY = unity_lazy;
+    SIZE = 1;
+    HEIGHT = 0;
+    while (SIZE < n) SIZE <<= 1, ++HEIGHT;
+    dat.assign(SIZE * 2, UNITY_MONOID);
+    lazy.assign(SIZE * 2, UNITY_LAZY);
+  }
+
+  void set(ll a, const Monoid &v) { dat[a + SIZE] = v; }
+  void build() {
+    for (ll k = SIZE - 1; k > 0; --k) dat[k] = FM(dat[k * 2], dat[k * 2 + 1]);
+  }
+
+  inline void evaluate(ll k) {
+    if (lazy[k] == UNITY_LAZY) return;
+    if (k < SIZE) FL(lazy[k * 2], lazy[k]), FL(lazy[k * 2 + 1], lazy[k]);
+    FA(dat[k], lazy[k]);
+    lazy[k] = UNITY_LAZY;
+  }
+
+  inline void update(ll a, ll b, const Action &v, ll k, ll l, ll r) {
+    evaluate(k);
+    if (a <= l && r <= b)
+      FL(lazy[k], v), evaluate(k);
+    else if (a < r && l < b) {
+      update(a, b, v, k * 2, l, (l + r) >> 1),
+          update(a, b, v, k * 2 + 1, (l + r) >> 1, r);
+      dat[k] = FM(dat[k * 2], dat[k * 2 + 1]);
+    }
+  }
+
+  /* [a, b) */
+  inline void update(ll a, ll b, const Action &v) {
+    update(a, b, v, 1, 0, SIZE);
+  }
+
+  inline Monoid get(ll a, ll b, ll k, ll l, ll r) {
+    evaluate(k);
+    if (a <= l && r <= b)
+      return dat[k];
+    else if (a < r && l < b)
+      return FM(get(a, b, k * 2, l, (l + r) >> 1),
+                get(a, b, k * 2 + 1, (l + r) >> 1, r));
+    else
+      return UNITY_MONOID;
+  }
+
+  /* [a, b) */
+  inline Monoid get(ll a, ll b) { return get(a, b, 1, 0, SIZE); }
+
+  inline Monoid operator[](ll a) { return get(a, a + 1); }
+};
+
+void solve() {
+  ll N, Q;
+  cin >> N >> Q;
+
+  auto fm = [](ll a, ll b) { return min(a, b); };
+  auto fa = [](ll &a, ll b) { a = min(a, b); };
+  auto fl = [](ll &a, ll b) { a = min(a, b); };
+  LazySegTree<ll, ll> row(N, fm, fa, fl, INF, INF);
+  LazySegTree<ll, ll> col(N, fm, fa, fl, INF, INF);
+
+  row.update(0, N, N - 2);
+  col.update(0, N, N - 2);
+
+  ll res = (N - 2) * (N - 2);
+
+  while (Q--) {
+    ll type, x;
+    cin >> type >> x;
+    x--;
+
+    if (type == 1) {
+      ll mi = col.get(x, x + 1);
+      res -= mi;
+      row.update(1, mi + 1, x - 1);
+    } else if (type == 2) {
+      ll mi = row.get(x, x + 1);
+      res -= mi;
+      col.update(1, mi + 1, x - 1);
+    }
+  }
+
+  cout << res << "\n";
+}
