ecaddb6dc1b0554e8e98383e75298798076c3bec
Toshinori Tsugita <tsugita.dev@gmail.com>
2020-09-27 02:48:34 +0900

    Solve practice2 k

---
+++ contests/practice2/practice2_k/main.cpp
@@ -0,0 +1,144 @@
+// https://atcoder.jp/contests/practice2/tasks/practice2_k
+
+#include <atcoder/all>
+
+#include "algorithm"
+#include "bitset"
+#include "cmath"
+#include "functional"
+#include "iomanip"
+#include "iostream"
+#include "map"
+#include "numeric"
+#include "queue"
+#include "set"
+#include "string"
+#include "vector"
+#define rep(i, to) for (ll i = 0; i < (to); ++i)
+#define rep1(i, to) for (ll i = 1; i <= (to); ++i)
+#define repf(i, from, to) for (ll i = from; i < (to); ++i)
+#define repr(i, from) for (ll i = from - 1; i >= 0; --i)
+#define all(vec) vec.begin(), vec.end()
+#define unless(cond) if (!(cond))
+#define fi first
+#define se second
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using ld = long double;
+template <typename T>
+using V = vector<T>;
+using VL = V<ll>;
+template <typename T>
+using VV = V<V<T>>;
+using VVL = VV<ll>;
+template <typename T>
+using VVV = VV<V<T>>;
+template <typename T, typename U>
+using P = pair<T, U>;
+using PL = P<ll, ll>;
+using VPL = V<PL>;
+template <typename T>
+using asc_pq = priority_queue<T, V<T>, greater<T>>;
+
+template <typename T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline ll len(V<T> arr) {
+  return arr.size();
+}
+
+struct exit_exception : public std::exception {
+  const char* what() const throw() { return "Exited"; }
+};
+
+template <typename T>
+void drop(T res) {
+  cout << res << endl;
+  throw exit_exception();
+}
+
+const ll INF = 1e18;
+
+void solve();
+
+#ifndef TEST
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  try {
+    solve();
+  } catch (exit_exception& e) {
+  }
+
+  return 0;
+}
+#endif
+
+using mint = modint998244353;
+
+struct S {
+  mint a;
+  int size;
+};
+
+struct F {
+  mint a, b;
+};
+
+S op(S l, S r) { return S{l.a + r.a, l.size + r.size}; }
+
+S e() { return S{0, 0}; }
+
+S mapping(F l, S r) { return S{r.a * l.a + r.size * l.b, r.size}; }
+
+F composition(F l, F r) { return F{r.a * l.a, r.b * l.a + l.b}; }
+
+F id() { return F{1, 0}; }
+
+void solve() {
+  ll N, Q;
+  cin >> N >> Q;
+
+  vector<S> as(N);
+  rep(i, N) {
+    ll x;
+    cin >> x;
+    as[i] = S{x, 1};
+  }
+
+  lazy_segtree<S, op, e, F, mapping, composition, id> seg(as);
+
+  rep(i, Q) {
+    ll t;
+    cin >> t;
+
+    if (t == 0) {
+      ll l, r, c, d;
+      cin >> l >> r >> c >> d;
+      seg.apply(l, r, F{c, d});
+    } else if (t == 1) {
+      ll l, r;
+      cin >> l >> r;
+      ll res = seg.prod(l, r).a.val();
+      cout << res << "\n";
+    }
+  }
+}
