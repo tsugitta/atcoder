ecfed746b4d65753bada483b84f28b83e9ba9d40
Toshinori Tsugita <tsugita.dev@gmail.com>
2020-11-01 20:58:22 +0900

    Solve arc106 a, b

---
+++ contests/arc106/arc106_a/main.cpp
@@ -0,0 +1,123 @@
+// https://atcoder.jp/contests/arc106/tasks/arc106_a
+
+#include <atcoder/all>
+
+#include "algorithm"
+#include "bitset"
+#include "cmath"
+#include "functional"
+#include "iomanip"
+#include "iostream"
+#include "map"
+#include "numeric"
+#include "queue"
+#include "set"
+#include "string"
+#include "vector"
+#define rep(i, to) for (ll i = 0; i < (to); ++i)
+#define rep1(i, to) for (ll i = 1; i <= (to); ++i)
+#define repf(i, from, to) for (ll i = from; i < (to); ++i)
+#define repr(i, from) for (ll i = from - 1; i >= 0; --i)
+#define all(vec) vec.begin(), vec.end()
+#define unless(cond) if (!(cond))
+#define fi first
+#define se second
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using ld = long double;
+template <typename T>
+using V = vector<T>;
+using VL = V<ll>;
+template <typename T>
+using VV = V<V<T>>;
+using VVL = VV<ll>;
+template <typename T>
+using VVV = VV<V<T>>;
+template <typename T, typename U>
+using P = pair<T, U>;
+using PL = P<ll, ll>;
+using VPL = V<PL>;
+template <typename T>
+using asc_pq = priority_queue<T, V<T>, greater<T>>;
+
+template <typename T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline ll len(V<T> arr) {
+  return arr.size();
+}
+
+struct exit_exception : public std::exception {
+  const char* what() const throw() { return "Exited"; }
+};
+
+template <typename T>
+void drop(T res) {
+  cout << res << endl;
+  throw exit_exception();
+}
+
+const ll INF = 1e18;
+
+void solve();
+
+#ifndef TEST
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  try {
+    solve();
+  } catch (exit_exception& e) {
+  }
+
+  return 0;
+}
+#endif
+
+ll powll(ll a, ll b) {
+  ll res = 1;
+
+  while (b--) {
+    if (res * a > 1e18) return 1e18;
+    res *= a;
+  }
+
+  return res;
+}
+
+void solve() {
+  ll N;
+  cin >> N;
+
+  rep1(a, 100) rep1(b, 100) {
+    ll ap = powll(3, a);
+    ll bp = powll(5, b);
+    if (ap > N) continue;
+    if (bp > N) continue;
+
+    if (ap + bp == N) {
+      cout << a << " " << b << "\n";
+      return;
+    }
+  }
+
+  cout << -1 << "\n";
+}
