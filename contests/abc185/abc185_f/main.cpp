b24758507a572d5117ed9a955f13cf883ef07c41
Toshinori Tsugita <tsugita.dev@gmail.com>
2020-12-15 09:05:05 +0900

    Solve abc185

---
+++ contests/abc185/abc185_f/main.cpp
@@ -0,0 +1,123 @@
+// https://atcoder.jp/contests/abc185/tasks/abc185_f
+
+#include <atcoder/all>
+
+#include "algorithm"
+#include "bitset"
+#include "cmath"
+#include "functional"
+#include "iomanip"
+#include "iostream"
+#include "map"
+#include "numeric"
+#include "queue"
+#include "set"
+#include "string"
+#include "vector"
+#define rep(i, to) for (ll i = 0; i < (to); ++i)
+#define rep1(i, to) for (ll i = 1; i <= (to); ++i)
+#define repf(i, from, to) for (ll i = from; i < (to); ++i)
+#define repr(i, from) for (ll i = from - 1; i >= 0; --i)
+#define all(vec) vec.begin(), vec.end()
+#define unless(cond) if (!(cond))
+#define fi first
+#define se second
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using ld = long double;
+template <typename T>
+using V = vector<T>;
+using VL = V<ll>;
+template <typename T>
+using VV = V<V<T>>;
+using VVL = VV<ll>;
+template <typename T>
+using VVV = VV<V<T>>;
+template <typename T, typename U>
+using P = pair<T, U>;
+using PL = P<ll, ll>;
+using VPL = V<PL>;
+template <typename T>
+using asc_pq = priority_queue<T, V<T>, greater<T>>;
+
+template <typename T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline ll len(V<T> arr) {
+  return arr.size();
+}
+
+struct exit_exception : public std::exception {
+  const char* what() const throw() { return "Exited"; }
+};
+
+template <typename T>
+void drop(T res) {
+  cout << res << endl;
+  throw exit_exception();
+}
+
+const ll INF = 1e18;
+
+void solve();
+
+#ifndef TEST
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  try {
+    solve();
+  } catch (exit_exception& e) {
+  }
+
+  return 0;
+}
+#endif
+
+// 演算
+ll op(ll a, ll b) { return a ^ b; }
+
+// 単位元
+ll e() { return 0; }
+
+void solve() {
+  ll N, Q;
+  cin >> N >> Q;
+
+  VL as(N);
+  rep(i, N) cin >> as[i];
+
+  segtree<ll, op, e> seg(as);
+
+  rep(q, Q) {
+    ll t, x, y;
+    cin >> t >> x >> y;
+    --x;
+
+    if (t == 1) {
+      ll n_x = seg.get(x);
+      n_x ^= y;
+      seg.set(x, n_x);
+    } else {
+      cout << seg.prod(x, y) << "\n";
+    }
+  }
+}
