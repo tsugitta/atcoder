6638893017fbe5f6896bca8b9f8a161386da4b54
Toshinori Tsugita <tsugita.dev@gmail.com>
2021-01-31 08:32:16 +0900

    Solve abc188 a ~ e

---
+++ contests/abc188/abc188_d/main.cpp
@@ -0,0 +1,130 @@
+// https://atcoder.jp/contests/abc188/tasks/abc188_d
+
+#include <atcoder/all>
+
+#include "algorithm"
+#include "bitset"
+#include "cmath"
+#include "functional"
+#include "iomanip"
+#include "iostream"
+#include "map"
+#include "numeric"
+#include "queue"
+#include "set"
+#include "string"
+#include "vector"
+#define rep(i, to) for (ll i = 0; i < (to); ++i)
+#define rep1(i, to) for (ll i = 1; i <= (to); ++i)
+#define repf(i, from, to) for (ll i = from; i < (to); ++i)
+#define repr(i, from) for (ll i = from - 1; i >= 0; --i)
+#define all(vec) vec.begin(), vec.end()
+#define unless(cond) if (!(cond))
+#define fi first
+#define se second
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using ld = long double;
+template <typename T>
+using V = vector<T>;
+using VL = V<ll>;
+template <typename T>
+using VV = V<V<T>>;
+using VVL = VV<ll>;
+template <typename T>
+using VVV = VV<V<T>>;
+template <typename T, typename U>
+using P = pair<T, U>;
+using PL = P<ll, ll>;
+using VPL = V<PL>;
+template <typename T>
+using asc_pq = priority_queue<T, V<T>, greater<T>>;
+
+template <typename T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline ll len(V<T> arr) {
+  return arr.size();
+}
+
+struct exit_exception : public std::exception {
+  const char* what() const throw() { return "Exited"; }
+};
+
+template <typename T>
+void drop(T res) {
+  cout << res << endl;
+  throw exit_exception();
+}
+
+const ll INF = 1e18;
+
+void solve();
+
+#ifndef TEST
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  try {
+    solve();
+  } catch (exit_exception& e) {
+  }
+
+  return 0;
+}
+#endif
+
+void solve() {
+  ll N, C;
+  cin >> N >> C;
+
+  ll ADD = 0;
+  ll REMOVE = 1;
+
+  struct Event {
+    ll day, val, type;
+  };
+
+  V<Event> events;
+
+  rep(i, N) {
+    ll a, b, c;
+    cin >> a >> b >> c;
+    events.push_back({a, c, ADD});
+    events.push_back({b + 1, c, REMOVE});
+  }
+
+  sort(all(events), [](Event lhs, Event rhs) { return lhs.day < rhs.day; });
+
+  ll last_price = 0;
+  ll last_day = 0;
+  ll res = 0;
+
+  for (auto e : events) {
+    res += (e.day - last_day) * min(C, last_price);
+
+    last_day = e.day;
+    if (e.type == ADD) last_price += e.val;
+    if (e.type == REMOVE) last_price -= e.val;
+  }
+
+  cout << res << "\n";
+}
