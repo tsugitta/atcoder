3671c1400792f3387ed652c184cd3c9623e5b459
Toshinori Tsugita <tsugita.dev@gmail.com>
2021-05-08 11:44:48 +0900

    Solve arc117 a ~ c

---
+++ contests/arc117/arc117_c/main.cpp
@@ -0,0 +1,196 @@
+// https://atcoder.jp/contests/arc117/tasks/arc117_c
+
+#include <atcoder/all>
+
+#include "algorithm"
+#include "bitset"
+#include "cmath"
+#include "functional"
+#include "iomanip"
+#include "iostream"
+#include "map"
+#include "numeric"
+#include "queue"
+#include "set"
+#include "string"
+#include "vector"
+#define rep(i, to) for (ll i = 0; i < ((ll)to); ++i)
+#define rep1(i, to) for (ll i = 1; i <= ((ll)to); ++i)
+#define repf(i, from, to) for (ll i = from; i < ((ll)to); ++i)
+#define repr(i, from) for (ll i = from - 1; i >= 0; --i)
+#define all(vec) vec.begin(), vec.end()
+#define unless(cond) if (!(cond))
+#define fi first
+#define se second
+#define i128 __int128_t
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using ld = long double;
+template <typename T>
+using V = vector<T>;
+using VL = V<ll>;
+template <typename T>
+using VV = V<V<T>>;
+using VVL = VV<ll>;
+template <typename T>
+using VVV = VV<V<T>>;
+template <typename T, typename U>
+using P = pair<T, U>;
+using PL = P<ll, ll>;
+using VPL = V<PL>;
+template <typename T>
+using asc_pq = priority_queue<T, V<T>, greater<T>>;
+
+template <typename T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline ll len(V<T> arr) {
+  return arr.size();
+}
+
+struct exit_exception : public std::exception {
+  const char* what() const throw() { return "Exited"; }
+};
+
+template <typename T>
+void drop(T res) {
+  cout << res << endl;
+  throw exit_exception();
+}
+
+const ll INF = 1e18;
+
+void solve();
+
+#ifndef TEST
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  try {
+    solve();
+  } catch (exit_exception& e) {
+  }
+
+  return 0;
+}
+#endif
+
+template <int MOD>
+class Fp {
+ public:
+  long long val;
+  constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
+    if (val < 0) val += MOD;
+  }
+  constexpr int getmod() { return MOD; }
+  constexpr Fp operator-() const noexcept { return val ? MOD - val : 0; }
+  constexpr Fp operator+(const Fp& r) const noexcept { return Fp(*this) += r; }
+  constexpr Fp operator-(const Fp& r) const noexcept { return Fp(*this) -= r; }
+  constexpr Fp operator*(const Fp& r) const noexcept { return Fp(*this) *= r; }
+  constexpr Fp operator/(const Fp& r) const noexcept { return Fp(*this) /= r; }
+  constexpr Fp& operator+=(const Fp& r) noexcept {
+    val += r.val;
+    if (val >= MOD) val -= MOD;
+    return *this;
+  }
+  constexpr Fp& operator-=(const Fp& r) noexcept {
+    val -= r.val;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr Fp& operator*=(const Fp& r) noexcept {
+    val = val * r.val % MOD;
+    return *this;
+  }
+  constexpr Fp& operator/=(const Fp& r) noexcept {
+    long long a = r.val, b = MOD, u = 1, v = 0;
+    while (b) {
+      long long t = a / b;
+      a -= t * b;
+      swap(a, b);
+      u -= t * v;
+      swap(u, v);
+    }
+    val = val * u % MOD;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr bool operator==(const Fp& r) const noexcept {
+    return this->val == r.val;
+  }
+  constexpr bool operator!=(const Fp& r) const noexcept {
+    return this->val != r.val;
+  }
+  friend constexpr ostream& operator<<(ostream& os, const Fp<MOD>& x) noexcept {
+    return os << x.val;
+  }
+  friend constexpr Fp<MOD> modpow(const Fp<MOD>& a, long long n) noexcept {
+    if (n == 0) return 1;
+    auto t = modpow(a, n / 2);
+    t = t * t;
+    if (n & 1) t = t * a;
+    return t;
+  }
+};
+
+using mint = static_modint<3>;
+
+void solve() {
+  ll N;
+  string S;
+  cin >> N >> S;
+
+  map<char, int> char_to_i = {{'B', 0}, {'W', 1}, {'R', 2}};
+  VL i_to_char = {'B', 'W', 'R'};
+
+  mint top_mod = 0;
+  ll comb = 1;
+
+  VL exps(N + 1);  // exps[num]: 素因数として持つ 3^p の p
+  for (ll base = 3; base <= N; base *= 3) {
+    for (ll num = base; num <= N; num += base) {
+      exps[num]++;
+    }
+  }
+
+  VL mods(N + 1);  // mods[num]: num / 3^exps[num] % 3
+  rep1(num, N) mods[num] = num / (ll)pow(3, exps[num]) % 3;
+
+  // C(n, r) % 3 を計算していく
+  ll comb_exp = 0;
+  mint comb_mod = 1;
+
+  rep(i, N) {
+    ll val = char_to_i[S[i]];
+    mint comb = comb_exp > 0 ? 0 : comb_mod;
+
+    top_mod += comb * val;
+    comb_exp += exps[N - 1 - i];
+    comb_mod *= mods[N - 1 - i];
+    comb_exp -= exps[i + 1];
+    comb_mod /= mods[i + 1];
+  }
+
+  if (N % 2 == 0) top_mod *= -1;
+
+  char res = i_to_char[top_mod.val()];
+  cout << res << "\n";
+}
